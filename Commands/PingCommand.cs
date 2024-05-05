bd432c478c78361e7a3074c4b9a372e90f053076
CSDotNET0211 <me@csdotnet.dev>
2024-05-05 19:30:57 +0900

    ping fix

--- Commands/PingCommand.cs
+++ Commands/PingCommand.cs
@@ -8,9 +8,9 @@ public class PingModule : ModuleBase<SocketCommandContext>
 	[Command("ping")]
 	public async Task EchoAsync([Remainder] string text = "")
 	{
-		DateTime dt = DateTime.ParseExact(Context.Message.CreatedAt.ToString(), "yyyy/MM/dd HH:mm:ss zzz", null);
-		TimeSpan diff = DateTime.Now - dt;
+	//	DateTime dt = DateTime.ParseExact(Context.Message.CreatedAt.ToString(), "yyyy/MM/dd HH:mm:ss zzz", null);
+	//	TimeSpan diff = DateTime.Now - dt;
 
-		await Context.Message.ReplyAsync($"pong! ({diff.TotalMilliseconds}ms) {Context.Message.CreatedAt.ToString()}");
+		await Context.Message.ReplyAsync($"pong! {Context.Message.CreatedAt.ToString()}");
 	}
 }
