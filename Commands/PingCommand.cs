eb8776d7e3429d250c772c5f074946f541cf0dd2
CSDotNET0211 <me@csdotnet.dev>
2024-05-05 19:29:30 +0900

    ping fix

--- Commands/PingCommand.cs
+++ Commands/PingCommand.cs
@@ -6,11 +6,11 @@ namespace Minomuncher.Commands;
 public class PingModule : ModuleBase<SocketCommandContext>
 {
 	[Command("ping")]
-	public async Task EchoAsync()
+	public async Task EchoAsync([Remainder] string text = "")
 	{
 		DateTime dt = DateTime.ParseExact(Context.Message.CreatedAt.ToString(), "yyyy/MM/dd HH:mm:ss zzz", null);
 		TimeSpan diff = DateTime.Now - dt;
 
-		await Context.Message.ReplyAsync($"pong! ({diff.Milliseconds}ms) {Context.Message.CreatedAt.ToString()}");
+		await Context.Message.ReplyAsync($"pong! ({diff.TotalMilliseconds}ms) {Context.Message.CreatedAt.ToString()}");
 	}
 }
